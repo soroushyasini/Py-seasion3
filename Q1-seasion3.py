9dc2cda7eede7e3737f163ddcb5a3d08a88499fe
soroushyasini <119971742+soroushyasini@users.noreply.github.com>
2022-12-28 21:37:23 +0330

    Update Q1-seasion3.py

--- Q1-seasion3.py
+++ Q1-seasion3.py
@@ -1,13 +1,17 @@
-
-array = []
-n = int(input("Enter how many numbers do you want to enter : "))
-
-print ("inter your number than press ENTER : ")
-for i in range(0, n):
-	ele = int(input())
-
-	array.append(ele)
-	
-print("here is your list : ", array)
-var = [*set(array)]
-print ("here is your var without duplication: ", var)
+def eq():
+    print("""the Standear form of Quadratic Equation is :
+           aX^2 + bX + c = O
+           to solve the problem, insert the following numbers : """)
+    a = int(input(" a : "))
+    b = int(input(" b : "))
+    c = int(input(" c : "))
+    delta = b ** 2 - 4 * a * c
+    if delta > 0 : 
+        x1 = ((-1 * b - (b ** 2 - 4 * a * c)** 0.5) / 2 * a )
+        x2 = ((-1 * b + (b ** 2 - 4 * a * c)** 0.5) / 2 * a )
+    if delta == 0 :
+        print (" the only answer is = " , x1 )
+    if delta < 0 :
+        print ("the equation has no answer." ) 
+         
+eq()
