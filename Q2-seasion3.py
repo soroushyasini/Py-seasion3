8f5dd39e693477256c7b1873f87cd51933bac84c
soroushyasini <119971742+soroushyasini@users.noreply.github.com>
2022-12-28 21:36:42 +0330

    Update Q2-seasion3.py

--- Q2-seasion3.py
+++ Q2-seasion3.py
@@ -1,7 +1,6 @@
 n = int(input("please insert the lines : "))
 m = int(input("please insert the columns : "))
-array = []
-array = [["#*" for j in range(m)] for i in range(n)]
+#array = []
+#array = [["#*" for j in range(m)] for i in range(n)]
 for i in range(n):
-    print(array[i])
-
+    print(m * "*#")
